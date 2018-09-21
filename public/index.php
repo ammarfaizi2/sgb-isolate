834f01e038dc8b51e167fd2fdb38c7f0e26a8a48
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-21 11:02:14 +0700

    Update isolator

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ define("ISOLATOR_HOME", ISOLATOR."/home");
 
 require __DIR__."/../isolated_files/Isolator.php";
 
-if (isset($_GET["code"])) {
+if (isset($_POST["code"])) {
 	$id = 1;
 	$st = new Isolator($id);
 	file_put_contents(
@@ -18,22 +18,35 @@ if (isset($_GET["code"])) {
 	exit;
 }
 
-
-
 if (! file_exists($f)) {
 	file_put_contents($f, $this->code);
 }
 
-$st->setMemoryLimit(1024 * 512);
-$st->setMaxProcesses(5);
-$st->setMaxWallTime(10);
-$st->setMaxExecutionTime(5);
-$st->setExtraTime(5);
+$st->setMemoryLimit(1024 * 512); // max memory usage per exec 512 MB
+$st->setMaxProcesses(5); // max child processes per exec 5 processes
+$st->setMaxWallTime(10); // max walltime per exec 10 seconds
+$st->setMaxExecutionTime(5); // max exec time per exec 5 seconds
+$st->setExtraTime(5); // max extratime per exec 5 seconds
+
 
 $st->run("/usr/bin/php7.1 /home/u".$id."/".$fn);
-
 
 print "ISOLATE OUT:<br/> <pre>".htmlspecialchars($st->getIsolateOut())."</pre>";
 print "<br/>STDOUT:<br/> <pre>".htmlspecialchars($st->getStdout())."</pre>";
 print "<br/>STDERR:<br/> <pre>".htmlspecialchars($st->getStderr())."</pre>";
 unset($st);
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>SGB Ngods</title>
+</head>
+<body>
+	<center>
+	<h3>Enter your PHP code here!</h3>
+	<form action="?action=1" method="POST">
+		<textarea name="code"><?php  print htmlspecialchars("<?php echo \"Hello SGB!\";"); ?></textarea>
+		<input type="submit" name="submit" value="Submit"/>
+	</form>
+	</center>
+</body>
+</html>
